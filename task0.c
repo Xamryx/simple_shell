a510b92be7f59e6045cadd7b9b77ac31cfbc9856
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-08-11 03:33:26 -0700

    Betty would be proud, If you us to have more comlicated one, just tell me and we will modify the file for it.

---
+++ task0.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+  * main - main function
+  *
+  * Return: Always 0 Success
+  */
+int main(void)
+{
+	printf("\t Simple Shell Project");
+	printf("\n");
+	printf("This is a Team Project\n");
+	printf("The team members are:\n");
+	printf("\t Gbeminiyi Babatunde and, \n Maman Sani Ibrahim");
+	printf("\n");
+
+	return (0);
+}
