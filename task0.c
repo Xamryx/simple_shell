a1c940995250ccc350ec6a6cf238fc9bb28dd949
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-08-22 16:35:21 -0700

    update task0 file

--- task0.c
+++ task0.c
@@ -7,11 +7,15 @@
   */
 int main(void)
 {
+	printf("########################");
+	printf("\n");
 	printf("\t Simple Shell Project");
 	printf("\n");
 	printf("This is a Team Project\n");
 	printf("The team members are:\n");
 	printf("\t Gbeminiyi Babatunde and, \n Maman Sani Ibrahim");
+	printf("\n");
+	printf("##########################");
 	printf("\n");
 
 	return (0);
