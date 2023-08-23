0b498a252f622a5d33610de61d3f014c56252686
Ibrahim227 <mamansani.ibrahim01@gmail.com>
2023-08-22 17:06:05 -0700

    update shell.h

--- shell.h
+++ shell.h
@@ -3,6 +3,14 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<limits.h>
+#include<fcntl.h>
+#include<errno.h>
+
 int main(void);
 
 #endif
